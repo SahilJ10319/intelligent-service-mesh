64d8df5ccc39491b6a8c0fdc28401c8f855c52a2
saadhaniftaj <your.email@example.com>
2026-01-28 03:17:51 +0500

    initial project setup

---
+++ src/main/java/com/neuragate/NeuraGateApplication.java
@@ -0,0 +1,13 @@
+package com.neuragate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Main entry point for NeuraGate
+@SpringBootApplication
+public class NeuraGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeuraGateApplication.class, args);
+    }
+}
